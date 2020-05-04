5f7dcedbeb5aec2543f3e6cfa5cfe6d92cbf47e5
Jessica <Jessica@users-iMac.local>
2020-05-04 14:45:20 -0400

    MVP

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 require('dotenv').config();
 
 const server = require('./server.js');
-const port = process.env.PORT; 
+// const port = process.env.PORT; 
+
+const port = 5000; 
 
 server.listen(port, () => {
   console.log(`\n *** Server listening on http://localhost:${port} *** \n`)
